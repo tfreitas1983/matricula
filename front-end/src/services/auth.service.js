72da7dc8a4b1f092c699718d83bfe2e281cb4e99
Thiago Freitas <63019037+tfreitas1983@users.noreply.github.com>
2021-10-26 16:55:38 -0300

    Update auth.service.js

--- front-end/src/services/auth.service.js
+++ front-end/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://matricula.ddns.net:8085/api/auth/";
+const API_URL = "http://10.1.1.26.net:8085/api/auth/";
 
 class AuthService {
   login(username, password) {
