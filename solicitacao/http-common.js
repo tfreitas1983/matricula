e8c757119c4a7a5dd9aa332e728847f281236011
Thiago Freitas <63019037+tfreitas1983@users.noreply.github.com>
2021-10-26 16:59:08 -0300

    Update http-common.js

--- solicitacao/http-common.js
+++ solicitacao/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://matricula.ddns.net:8085/api",
+    baseURL: "http://10.1.1.26:8085/api",
+   
     headers: {
         "Content-type": "application/json"
     }
